c9ab002dacffb1a2a96b5e82415cb23c0e0a9c85
Петров Олег <araragikyn@hotmail.com>
2020-02-06 10:22:31 +0800

    Описал поля и конструкторы для структур Owner

---
+++ HomeWork_07/Owner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_07
+{
+    struct Owner
+    {
+        /// <summary>
+        /// Имя пользователя 
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Дата рождения 
+        /// </summary>
+        public DateTime DateOfBirth { get; }
+        /// <summary>
+        /// Конструктор по всем полям
+        /// </summary>
+        /// <param name="name">Имя пользователя</param>
+        /// <param name="dateofbirth">Дата рождения</param>
+        public Owner (string name,DateTime dateofbirth)
+        {
+            this.Name = name;
+            this.DateOfBirth = dateofbirth;
+        }
+        /// <summary>
+        /// Перегрузка конструктора с 1 полем
+        /// </summary>
+        /// <param name="name">Имя пользователя </param>
+        public Owner(string name) :
+            this(name, new DateTime(1900, 1, 1))
+            {}
+
+
+    }
+}
---
+++ HomeWork_07/Record.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_07
+{
+    struct Record
+    {
+        /// <summary>
+        /// Заголовок записи
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// Важность
+        /// </summary>
+        public string Relevance { get; set; }
+        /// <summary>
+        /// Текст записи
+        /// </summary>
+        public string Text { get; set; }
+        /// <summary>
+        /// Дата записи
+        /// </summary>
+        public DateTime WriteDate { get; set; }
+
+        /// <summary>
+        /// Пользователь оставивший запись 
+        /// </summary>
+        public Owner RecordOwner { get;}
+
+        /// <summary>
+        /// Конструктор создающий запись
+        /// </summary>
+        /// <param name="title">Заголовок записи</param>
+        /// <param name="relevance">Важность записи</param>
+        /// <param name="text">Текст записи</param>
+        /// <param name="writeDate">Дата записи</param>
+        /// <param name="recordOwner">Пользователь оставивший</param>
+        public Record (string title, string relevance, string text, DateTime writeDate, Owner recordOwner )
+        {
+            this.Title = title;
+            this.Relevance = relevance;
+            this.Text = text;
+            this.WriteDate = writeDate;
+            this.RecordOwner = recordOwner;
+        }
+    }
+}

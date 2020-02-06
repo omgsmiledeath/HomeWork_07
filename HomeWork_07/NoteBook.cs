634dcf7a2ec6e1abe745fed45ac7b292f22b4e2d
Петров Олег <araragikyn@hotmail.com>
2020-02-06 11:05:01 +0800

    Добавил свойства и конструкторы для структуры записной книги ( методы не описаны)

---
+++ HomeWork_07/NoteBook.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_07
+{
+    class NoteBook
+    {
+        /// <summary>
+        /// Перечисление метода записи
+        /// </summary>
+        public enum WriteMode
+        {
+            Save,  //Сохранить в текущем файле
+            SaveAll //Сохранить в новом
+        }
+        /// <summary>
+        /// Массив записей 
+        /// </summary>
+        private List<Record> Records { get; set; }
+        /// <summary>
+        /// Путь до файла с записями
+        /// </summary>
+        private string path;
+
+        /// <summary>
+        /// Заголовки для вывода на экран
+        /// </summary>
+        private string[] titles;
+
+        /// <summary>
+        /// Конструктор для создания новой записной книги
+        /// </summary>
+        public NoteBook()
+        {
+            this.Records = new List<Record>();
+            this.titles = new string[0];
+        }
+
+        /// <summary>
+        /// Конструктор для открытия записной книги которая уже была создана
+        /// </summary>
+        /// <param name="path">путь до файла с записями</param>
+        public NoteBook (string path):
+            this()
+        {
+            this.path = path;
+            this.Load();
+        }
+
+        /// <summary>
+        /// Метод который загружает записи из файла 
+        /// </summary>
+        private void Load()
+        {
+
+        }
+
+        /// <summary>
+        /// Метод сохраняющий записную книгу
+        /// </summary>
+        /// <param name="wm">Переменная выбора способа сохранения</param>
+        private void Save(WriteMode wm)
+        {
+            if(wm == WriteMode.Save)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
